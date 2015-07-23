4961daed8efa90231b9c9f0b69f561dd13f138fe
paulwallis42 <paulwallis42@gmail.com>
2015-07-23 09:48:11 +0100

    Numbers div by 3, 5 or 15 return correct statement

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -52,5 +52,17 @@ describe('Javabuzz', function () {
     it('"Java" when a number is divisible by 3', function(){
       expect(javabuzz.says(3)).toEqual("Java");
     });
+
+    it('"Buzz" when a number is divsible by 5', function(){
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"Javabuzz" when a number is divisible by 15', function(){
+      expect(javabuzz.says(15)).toEqual("Javabuzz");
+    });
+
+    it('returns the number when not divisible by 3, 5, or 15', function(){
+      expect(javabuzz.says(7)).toEqual(7);
+    });
   });
 });
